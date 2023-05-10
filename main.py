89276caecaa927128037c4c88d2d48b09405b8c1
Mike1ife <132564989+Mike1ife@users.noreply.github.com>
2023-05-10 11:47:31 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import csv
 import argparse
 from preprocess import *
 
-def get_argument():
+def Get_Argument():
     parser = argparse.ArgumentParser()
     parser.add_argument('--read-csv', action='store_true', help='to read notes from csv file, default=False')
     parser.set_defaults(read_csv=False)
@@ -11,18 +11,8 @@ def get_argument():
 
 if __name__ == '__main__':
     # determine whether to get notes from csv and remove frequency
-    read_csv = get_argument()
-
-    Notes_List = []
-    if read_csv:
-        # read notes from csv
-        with open("Notes.csv", "r") as f:
-            reader = csv.reader(f, delimiter="\n")
-            for Notes in reader:
-                Notes_List.append(Notes[0])
-    else:
-        # load midi files
-        Midi_List = Load_Dataset()
-        # extract notes and saves in Notes.csv
-        Notes_List = Notes_Extraction(Midi_List)
-
+    read_csv = Get_Argument()
+    # get the list of notes of each song
+    Notes_List = Load_Dataset(read_csv)
+    # map note to index
+    Notes_Index_List = Notes_to_Index(Notes_List)
--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,5 @@
 import os
+import csv
 import numpy as np
 import pandas as pd
 from music21 import *
@@ -6,18 +7,26 @@ from collections import Counter
 import warnings
 warnings.filterwarnings('ignore')
 
-def Load_Dataset():
-    Midi_List = []
-    for dir in os.listdir('./Data_p'):
-        path = f'./Data_p/{dir}'
-        for file in os.listdir(path):
-            filepath = path + f'/{file}'
-            print(filepath)
-            # Load data from midi file
-            midi = converter.parse(filepath)
-            Midi_List.append(midi)
-
-    return Midi_List
+def Load_Dataset(read_csv):
+    if read_csv:
+        Notes_List = []
+        with open("Notes.csv", "r") as f:
+            reader = csv.reader(f, delimiter="\n")
+            for Notes in reader:
+                Notes = Notes[0].split()
+                Notes_List.append(Notes)
+        return Notes_List
+    else:
+        Midi_List = []
+        for dir in os.listdir('./Data'):
+            path = f'./Data/{dir}'
+            for file in os.listdir(path):
+                filepath = path + f'/{file}'
+                print(filepath)
+                # Load data from midi file
+                midi = converter.parse(filepath)
+                Midi_List.append(midi)
+        return Notes_Extraction(Midi_List)
 
 def Notes_Extraction(Midi_List):
     Notes_List = []
@@ -29,13 +38,16 @@ def Notes_Extraction(Midi_List):
             # get access to each element
             pick = part.recurse()
             for element in pick:
+                # note[0]=Name, note[-1]=octave
+                # note[1]=Flat or Sharp if have any
                 if isinstance(element, note.Note):
                     # if this element is note, then get its pitch
                     Notes.append(str(element.pitch))
                 elif isinstance(element, chord.Chord):
-                    # if this element is chord, get list of order
-                    # ex: chord=['c', 'e', 'g'], list=[0, 4, 7]
-                    Notes.append(".".join(str(n) for n in element.normalOrder))
+                    # if this element is chord, get list of pitches and join with '.'
+                    Notes.append(".".join(str(n.nameWithOctave) for n in element.pitches))
+                elif isinstance(element, note.Rest):
+                    Notes.append('REST')
         if Notes:
             Notes_List.append(' '.join(Notes))
 
@@ -43,3 +55,25 @@ def Notes_Extraction(Midi_List):
     df = pd.DataFrame(Notes_List)
     df.to_csv('Notes.csv', index=False, header=False)
     return Notes_List
+
+def Notes_to_Index(Notes_List):
+    Notes_vocab = [] # all vocabulary of notes
+    Notes_index = [] # list of note index of each song
+    Notes_map = {} # map from note to index
+
+    # list of all unique note from dataset
+    unique_note = Counter(Note for Notes in Notes_List for Note in set(Notes))
+    for Note in unique_note.keys():
+        Notes_vocab.append(Note)
+    
+    # create map
+    Notes_map = dict(sorted({k: v for v, k in enumerate(Notes_vocab)}.items(), key=lambda item: item[1]))
+
+    # mapping
+    for Notes in Notes_List:
+        index = []
+        for Note in Notes:
+            index.append(Notes_map[Note])
+        Notes_index.append(index)
+
+    return Notes_index
