bc615dce02092bc384d757a8b44da6d639cf5fcc
yuzheng91 <116017470+yuzheng91@users.noreply.github.com>
2023-05-28 18:30:55 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import numpy as np
+import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM, Activation
 from keras.utils import np_utils
@@ -38,11 +39,11 @@ def train(model, network_input, network_output):
     )
     callbacks_list = [checkpoint]
 
-    model.fit(network_input, network_output, epochs=1, batch_size=64, callbacks=callbacks_list)
+    model.fit(network_input, network_output, epochs=10, batch_size=64, callbacks=callbacks_list)
 
 def prepare_sequences(notes, n_vocab):
     """ Prepare the sequences used by the Neural Network """
-    sequence_length = 100
+    sequence_length = 1000
     network_input = []
     network_output = []
 
@@ -51,7 +52,6 @@ def prepare_sequences(notes, n_vocab):
         for i in range(0, len(song) - sequence_length, 1):
             sequence_in = song[i:i + sequence_length]
             sequence_out = song[i + sequence_length]
-            network_input.append([note / float(n_vocab) for note in sequence_in])
             network_output.append(sequence_out)
 
     n_patterns = len(network_input)
@@ -80,9 +80,9 @@ if __name__ == '__main__':
     # get the list of notes of each song
     Notes_List = Load_Dataset(read_csv)
     # map note to index
-    Notes_Index_List = Notes_to_Index(Notes_List)
+    Notes_Index_List, Notes_map = Notes_to_Index(Notes_List)
 
-    n_vocab = len(set([item for sublist in Notes_List for item in sublist]))
+    n_vocab = len(Notes_map)
 
     network_input, network_output = prepare_sequences(Notes_Index_List, n_vocab)
 
